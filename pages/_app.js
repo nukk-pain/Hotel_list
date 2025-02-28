7c444d3967f658942a7d93c851842d14337a886f
nukk-pain <nukk_pain@nukk.com>
2025-02-28 17:06:31 +0900

    _app.js 오류 수정

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,7 @@ import '../styles/globals.css';
 import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
-    return
-    <Component {...pageProps} />
-        ;
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
